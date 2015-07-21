57580bb9fdf786fefac16848d647cbdaa451416b
Wes <wesley.ch.yu@gmail.com>
2015-07-21 13:05:29 -0700

    Add User Model with Password Validation using Bcrypt

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+
+  has_secure_password
+
+  validates :email,
+    presence: true,
+    uniqueness: true
+
+  validates :firstname,
+    presence: true
+
+  validates :lastname,
+    presence :true
+
+  validates :password,
+    length: {in: 6..20}, on: :create
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721040232) do
+ActiveRecord::Schema.define(version: 20150721195707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20150721040232) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
